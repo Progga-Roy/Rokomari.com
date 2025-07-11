ecfac6b13f97379dcd67e722779bbf9c16c7116e
Progga-Roy <proggaroy2000@gmail.com>
2025-07-12 00:01:34 +0600

    Find auther from the slider

--- src/test/java/pages/BookPage.java
+++ src/test/java/pages/BookPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 public class BookPage extends BasePage{
     public String url = "https://www.rokomari.com/";
     public String authorSectionTitle = "জনপ্রিয় লেখকগণ";
+    public String authorNameTitle = "হুমায়ূন আহমেদ";
     //    public String url = "https://www.rokomari.com/book?ref=nm";
     public By bookPagePath = By.xpath("(//span[contains(text(),'বই')])[2]");
     //    public By authors = By.xpath("(//h3[normalize-space()='Authors']");
@@ -18,6 +19,8 @@ public class BookPage extends BasePage{
 //    public By author = By.xpath("(//a[@id='js--authors'])[1]");
 public By authorsTab = By.xpath("//a[contains(text(),'লেখক')]");
 public By authorSection = By.xpath("//h1[contains(text(),'জনপ্রিয় লেখকগণ')]");
+public By previousArrow = By.xpath("//button[normalize-space()='Previous']");
+public By nextArrow = By.xpath("//button[normalize-space()='Next']");
 
     public By authorName = By.xpath("//div[@class='authorListItem slick-slide slick-active']//a//div//h2[@class='small-sized-text text-center name-text-container'][contains(text(),'হুমায়ূন আহমেদ')]");
     //button[@class='modal_modalCloseButton__y_s5N']//*[name()='svg']//*[name()='path' and contains(@fill-rule,'evenodd')]
--- src/test/java/testCases/TestBookPage.java
+++ src/test/java/testCases/TestBookPage.java
@@ -1,46 +1,90 @@
 package testCases;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.BookPage;
 import pages.HomePage;
 import utilities.DriverSetUp;
 
+import java.util.List;
+
 public class TestBookPage extends DriverSetUp {
     HomePage home_page = new HomePage();
     BookPage book_page = new BookPage();
+
     @Test
     public void TestSelectAuthor() throws InterruptedException {
+//Load the page and handle modal if present
         home_page.loadAPage(home_page.url);
         Thread.sleep(3000);
         home_page.handleModalIfPresent();
+//Click on "Books" from homepage
         book_page.clickOnElement(book_page.bookPagePath);
-//      book_page.loadAPage(book_page.url);
         Thread.sleep(3000);
+//Open the "লেখক" (Authors) tab  and click
         book_page.waitForElement(book_page.authorsTab);
         book_page.clickOnElement(book_page.authorsTab);
 //        home_page.handleModalIfPresent();
+ //Scroll the "জনপ্রিয় লেখকগণ" section
         book_page.waitForElement(book_page.authorSection);
-        Assert.assertEquals(book_page.getElementByText(book_page.authorSection),book_page.authorSectionTitle);
-        System.out.println(book_page.authorSectionTitle);
+        Assert.assertEquals(book_page.getElementByText(book_page.authorSection), book_page.authorSectionTitle);
+        WebElement section = book_page.getElement(book_page.authorSection);
+        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView({block:'center'})", section);
+// Try find the Author : "হুমায়ূন আহমেদ"
+        for (int i = 0; i < 10; i++) {
+            List<WebElement> authors = getDriver().findElements(book_page.authorName);
+            if (!authors.isEmpty()) {
+                authors.get(0).click();
+                System.out.println("Click Author : " + book_page.authorNameTitle);
+                return;
+            }
+// If not fount  Author : "হুমায়ূন আহমেদ" in the first phase than  click arrow (next arrow) from the slider.
+            WebElement nextArrowBtn = book_page.getElement(book_page.nextArrow);
+            if (nextArrowBtn.isDisplayed() && nextArrowBtn.isEnabled()) {
+                nextArrowBtn.click();
+            } else {
+                break;
+            }
 
-//     book_page.waitForElement(book_page.authorName);
-//        home_page.handleModalIfPresent();
-//        // find author name ("হুমায়ূন আহমেদ")
-//        book_page.scrollElement(book_page.authorName);
-//       book_page.waitForElement(book_page.authorName);
-//       book_page.clickOnElement(book_page.authorName);
-//       book_page.writeOnElement(book_page.inputBox,"হুমায়ূন আহমেদ");
-//       book_page.waitForElement(book_page.textLabel);
-//       book_page.clickOnElement(book_page.textLabel);
-//        Thread.sleep(3000);
-//       home_page.scrollElement(book_page.bookPagePath);
-//        Thread.sleep(3000);
-//       book_page.waitForElement(book_page.bookNamePath);
-//       book_page.clickOnElement(book_page.bookPagePath);
-
-
+        }
 
     }
 
 }
+
+
+//
+//
+//WebElement section = book_page.getElement(book_page.authorSection);
+//        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView({block:'center'});", section);
+//        Thread.sleep(1000);
+//
+/// / Step 5: Try to find and click author "হুমায়ূন আহমেদ" (within 10 scrolls)
+//        for (int i = 0; i < 10; i++) {
+//List<WebElement> authors = getDriver().findElements(book_page.authorName); // Dynamic search
+//
+//            if (!authors.isEmpty()) {
+//        authors.get(0).click();
+//                System.out.println("Clicked on author: হুমায়ূন আহমেদ");
+//                return;
+//                        }
+//
+//                        // Scroll the slider using Next arrow if author not found
+//                        book_page.waitForElement(book_page.nextArrow);
+//WebElement nextArrowBtn = book_page.getElement(book_page.nextArrow);
+//            if (nextArrowBtn.isDisplayed() && nextArrowBtn.isEnabled()) {
+//        nextArrowBtn.click();
+//                Thread.sleep(1000);
+//            } else {
+//                    break;
+//                    }
+//                    }
+//
+//                    // Step 6: If author is still not found
+//                    System.out.println("Author not found: হুমায়ূন আহমেদ");
+//
+//
+
